09e302b7bf0092072ece43528c2690a6370256b0
Ramya2020 <51980775+Ramya2020@users.noreply.github.com>
2019-07-01 14:26:58 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+l=input()
+if l in ('a','e','i','o','u')
+  print("Vowel")
+elif l in('b','c','d','f','g','h','j','k','l','m,','n','p','q','r,'s','t',v','w','x','y','z'):
+  print("Cosonant")
+else:
+  print("invalid")
