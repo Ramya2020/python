5ded6543c87dbdb184bbf1fbbfe50b2972f5f839
Ramya2020 <51980775+Ramya2020@users.noreply.github.com>
2019-07-01 14:36:06 +0530

    Create vc.py

---
+++ vc.py
@@ -0,0 +1,7 @@
+s=input()
+if s in('a','e','i','o','u'):
+  print("Vowel")
+elif s in ('b','c','f','g','h','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+  print("Consonant")
+else:
+  print("invalid")
