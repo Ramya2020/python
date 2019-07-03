1f0796fdfa9261b6e4637684cb10c566bb772578
Ramya2020 <51980775+Ramya2020@users.noreply.github.com>
2019-07-03 16:44:43 +0530

    Update 14th.py

--- 14th.py
+++ 14th.py
@@ -1,4 +1,4 @@
-x,y=[int(x)for x in input().split()]
-for i in range (x+1,y+1):
+x1,y1=[int(x1)for x1in input().split()]
+for i in range (x1+1,y1+1):
   if(i%2!=0):
-    print(i,end="")
+    print(i,end=" ")
