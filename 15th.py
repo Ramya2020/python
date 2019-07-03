3aa59205abbb4c1c969e683d0951387247ba9f77
Ramya2020 <51980775+Ramya2020@users.noreply.github.com>
2019-07-03 16:53:21 +0530

    Update 15th.py

--- 15th.py
+++ 15th.py
@@ -1,12 +1,4 @@
-x1,y1 =map(int,input(" ").split())
-i=x1+1
-while(i<y1):
-    f=0
-    for j in range(2,i):
-        if(i%j==0):
-            f=1
-            break
-        j+=1
-    if(f==0):
-        print(i,end=" ")
-    i+=1
+r1,r2=[int(x) for x in input().split()]
+for r in range(r1+1,r2):
+ if(r%2==0):
+  print(r,end=" ")
