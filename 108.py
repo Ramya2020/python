0af7877fec588fe04aa5d5ca87763fd9215b16cc
Ramya2020 <51980775+Ramya2020@users.noreply.github.com>
2019-07-08 07:11:14 -0700

    Update 108.py

--- 108.py
+++ 108.py
@@ -1,7 +1,7 @@
-j,o=map(int,input().split())
-q=list(map(int,input().split()[:o]))
+j1,o1=map(int,input().split())
+q1=list(map(int,input().split()[:o1]))
 z=[]
-for i in q:
+for i in q1:
    if(i<=i+1):
      z.append(i)
-print(z[o-1])   
+print(z[o1-1])   
