34767975c071de68497936604b85ec6ce3f33638
Ramya2020 <51980775+Ramya2020@users.noreply.github.com>
2019-07-12 06:51:41 -0700

    Update pro10.py

--- pro10.py
+++ pro10.py
@@ -1,7 +1,7 @@
-chat=int(input())
+ch1=int(input())
 chot=[int(o) for o in input().split(" ")]
 chet=0
-for p in range(chat):
+for p in range(ch1):
       for g in range(p):
            if(chot[g]<chot[p]):
                 chet+=chot[g]
