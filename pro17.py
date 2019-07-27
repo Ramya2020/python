db853da3f043e3b3e92b6d384032a8a152e4d7d4
Ramya2020 <51980775+Ramya2020@users.noreply.github.com>
2019-07-27 09:48:40 +0530

    Update pro17.py

--- pro17.py
+++ pro17.py
@@ -1,9 +1,9 @@
 b,sent1 = input().split()
-sent1 = int(sent)
+sent1 = int(sent1)
 fake = False
 bent = list(map(int,input().split()))
 for i in range(len(bent)):
     for j in range(len(bent)):
-        if bent[i]+bent[j] == sent:
+        if bent[i]+bent[j] == sent1:
             fake = True
 print("yes" if fake else "no") 
