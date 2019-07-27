23d24ab8257754db26e937a63637e18915d6e8ef
Ramya2020 <51980775+Ramya2020@users.noreply.github.com>
2019-07-27 09:54:17 +0530

    Update pro18.py

--- pro18.py
+++ pro18.py
@@ -1,11 +1,11 @@
-nit,mit=map(int,input().split())
+nit1,mit1=map(int,input().split())
 tem=[]
 x=0
-for m in range(nit):
+for m in range(nit1):
     tem.append(list(map(int,input().split())))   
-for m in range(nit):
-    for j in range(mit-1):
-        for k in range(j+1,mit+1):
+for m in range(nit1):
+    for j in range(mit1-1):
+        for k in range(j+1,mit1+1):
             if tem[m][j:k]==[1]*len(tem[m][j:k]):
                  if all(tem[p+m][j:k]==[1]*len(tem[p+m][j:k]) for p in range(len(tem[m][j:k])-1)):
                      if len(tem[m][j:k])>x:
