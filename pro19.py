a18447ccb9926e0d015cc20e980835b9e5afab5d
Ramya2020 <51980775+Ramya2020@users.noreply.github.com>
2019-07-27 09:58:31 +0530

    Create pro19.py

---
+++ pro19.py
@@ -0,0 +1,11 @@
+count=int(input())
+array=[]
+ss1=[]
+for i in range(count):
+    array.append(list(map(int,input().split())))
+for llist in array:
+    for num in llist:
+        ss1.append(num)
+ss1.sort()
+for i in ss1:
+    print(i,end=' ')
