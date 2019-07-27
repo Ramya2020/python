7c1916e6ccb337495c24ce0f71b6e13a8f22b70e
Ramya2020 <51980775+Ramya2020@users.noreply.github.com>
2019-07-27 09:28:30 +0530

    Update pro15.py

--- pro15.py
+++ pro15.py
@@ -1,10 +1,10 @@
-law=input()
-yaw=map(int,input().split())
-paw=[]
-for g in yaw:
+la1=input()
+ya1=map(int,input().split())
+pa1=[]
+for g in ya1:
     enum=bin(g)
-    paw.append(enum)
-fraud=sorted(paw)
+    pa.append(enum)
+fraud=sorted(pa1)
 fraud.reverse()
 for h in fraud:
     print(int(h,2))
